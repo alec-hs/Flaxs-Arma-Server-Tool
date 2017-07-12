95540db8946a6d2f227dfa95c607e50bf227d139
alec-hs <alec-hs@users.noreply.github.com>
2017-07-12 10:18:19 +0200

    Stop adding duplicate mods.
    Fixes #4

--- Flax's-Arma-Server-Tool/ImportMod.vb
+++ Flax's-Arma-Server-Tool/ImportMod.vb
@@ -13,64 +13,78 @@ Public Class ImportMod
     End Sub
 
     Public Sub ImportModToList(modUrl As String)
-        Dim modID As Integer
+        Dim modID As Integer = modUrl.Substring(modUrl.IndexOf("?id=") + 4)
         Dim modName As String
         Dim appName As String
         Dim sourceString As String
+        Dim duplicate As Boolean = False
 
-        Try
-            sourceString = New System.Net.WebClient().DownloadString(modUrl)
+        Dim lines As List(Of String) = System.IO.File.ReadAllLines(modsfile).ToList
 
-            appName = sourceString.Substring(sourceString.IndexOf("content=" & ControlChars.Quote & "Steam Workshop:") + 25, 6)
-
-
-            If appName Like "Arma 3" Then
-                modName = sourceString.Substring(sourceString.IndexOf("<title>Steam Workshop :: ") + 25)
-                modName = StrReverse(modName)
-                modName = modName.Substring(modName.IndexOf(">eltit/<") + 8)
-                modName = StrReverse(modName)
-
-
-                If modUrl Like "http*://steamcommunity.com/*/filedetails/?id=*" Then
-                    Try
-                        modID = modUrl.Substring(modUrl.IndexOf("?id=") + 4)
-                        Dim steamUpdate As New DateTime(1970, 1, 1, 0, 0, 0)
-                        Dim modLine As String = modID & "," & modName & ",Not Installed," & steamUpdate & ",public" & Environment.NewLine
-
-                        If My.Computer.FileSystem.FileExists(modsfile) Then
-                            Dim fs As StreamWriter = File.AppendText(modsfile)
-
-                            fs.Write(modLine)
-                            fs.Close()
-
-                            Me.Close()
-                            MainWindow.UpdateModGrid()
-                        Else
-                            Dim fs As FileStream = File.Create(modsfile)
-
-                            Dim modInfo As Byte() = New UTF8Encoding(True).GetBytes(modLine)
-                            fs.Write(modInfo, 0, modInfo.Length)
-                            fs.Close()
-
-                            Me.Close()
-                            MainWindow.UpdateModGrid()
-                        End If
-                    Catch ex As Exception
-                        modID = Nothing
-                        MessageBox.Show("Please remove any charcters after the mod ID in the url.")
-                        MsgBox("An exception occurred:" & vbCrLf & ex.Message)
-                    End Try
-                Else
-                    MessageBox.Show("Please use format: https://steamcommunity.com/sharedfiles/filedetails/?id=*********")
-                End If
-            Else
-                MessageBox.Show("This is a workshop Item for a different game.")
-                Me.Close()
+        For Each line In lines
+            Dim values() As String = line.Split(",")
+            If modID = values(0) Then
+                duplicate = True
             End If
-        Catch ex As Exception
-            MessageBox.Show("Not a Valid URL.")
-            MsgBox("An exception occurred:" & vbCrLf & ex.Message)
-        End Try
+        Next
+
+        If Not duplicate Then
+            Try
+                sourceString = New System.Net.WebClient().DownloadString(modUrl)
+
+                appName = sourceString.Substring(sourceString.IndexOf("content=" & ControlChars.Quote & "Steam Workshop:") + 25, 6)
+
+
+                If appName Like "Arma 3" Then
+                    modName = sourceString.Substring(sourceString.IndexOf("<title>Steam Workshop :: ") + 25)
+                    modName = StrReverse(modName)
+                    modName = modName.Substring(modName.IndexOf(">eltit/<") + 8)
+                    modName = StrReverse(modName)
+
+
+                    If modUrl Like "http*://steamcommunity.com/*/filedetails/?id=*" Then
+                        Try
+                            Dim steamUpdate As New DateTime(1970, 1, 1, 0, 0, 0)
+                            Dim modLine As String = modID & "," & modName & ",Not Installed," & steamUpdate & ",public" & Environment.NewLine
+
+                            If My.Computer.FileSystem.FileExists(modsfile) Then
+                                Dim fs As StreamWriter = File.AppendText(modsfile)
+
+                                fs.Write(modLine)
+                                fs.Close()
+
+                                Me.Close()
+                                MainWindow.UpdateModGrid()
+                            Else
+                                Dim fs As FileStream = File.Create(modsfile)
+
+                                Dim modInfo As Byte() = New UTF8Encoding(True).GetBytes(modLine)
+                                fs.Write(modInfo, 0, modInfo.Length)
+                                fs.Close()
+
+                                Me.Close()
+                                MainWindow.UpdateModGrid()
+                            End If
+                        Catch ex As Exception
+                            modID = Nothing
+                            MessageBox.Show("Please remove any charcters after the mod ID in the url.")
+                            MsgBox("An exception occurred:" & vbCrLf & ex.Message)
+                        End Try
+                    Else
+                        MessageBox.Show("Please use format: https://steamcommunity.com/sharedfiles/filedetails/?id=*********")
+                    End If
+                Else
+                    MessageBox.Show("This is a workshop Item for a different game.")
+                    Me.Close()
+                End If
+            Catch ex As Exception
+                MsgBox("An exception occurred:" & vbCrLf & ex.Message)
+            End Try
+        Else
+            Me.Close()
+            MsgBox("Mod already imported.")
+        End If
+
     End Sub
 
 End Class
--- Flax's-Arma-Server-Tool/ImportPrivateMod.vb
+++ Flax's-Arma-Server-Tool/ImportPrivateMod.vb
@@ -15,32 +15,47 @@ Public Class ImportPrivateMod
         Else
             Dim modID As Integer = workshopIDBox.Text
             Dim modName As String = modNameBox.Text
+            Dim duplicate As Boolean = False
 
-            Try
-                Dim steamUpdate As New DateTime(1970, 1, 1, 0, 0, 0)
-                Dim modLine As String = modID & "," & modName & ",Not Installed," & steamUpdate & ",private" & Environment.NewLine
+            Dim lines As List(Of String) = System.IO.File.ReadAllLines(modsfile).ToList
 
-                If My.Computer.FileSystem.FileExists(modsfile) Then
-                    Dim fs As StreamWriter = File.AppendText(modsfile)
-
-                    fs.Write(modLine)
-                    fs.Close()
-
-                    Me.Close()
-                    MainWindow.UpdateModGrid()
-                Else
-                    Dim fs As FileStream = File.Create(modsfile)
-
-                    Dim modInfo As Byte() = New UTF8Encoding(True).GetBytes(modLine)
-                    fs.Write(modInfo, 0, modInfo.Length)
-                    fs.Close()
-
-                    Me.Close()
-                    MainWindow.UpdateModGrid()
+            For Each line In lines
+                Dim values() As String = line.Split(",")
+                If modID = values(0) Then
+                    duplicate = True
                 End If
-            Catch ex As Exception
+            Next
+            If Not duplicate Then
+                Try
+                    Dim steamUpdate As New DateTime(1970, 1, 1, 0, 0, 0)
+                    Dim modLine As String = modID & "," & modName & ",Not Installed," & steamUpdate & ",private" & Environment.NewLine
 
-            End Try
+                    If My.Computer.FileSystem.FileExists(modsfile) Then
+                        Dim fs As StreamWriter = File.AppendText(modsfile)
+
+                        fs.Write(modLine)
+                        fs.Close()
+
+                        Me.Close()
+                        MainWindow.UpdateModGrid()
+                    Else
+                        Dim fs As FileStream = File.Create(modsfile)
+
+                        Dim modInfo As Byte() = New UTF8Encoding(True).GetBytes(modLine)
+                        fs.Write(modInfo, 0, modInfo.Length)
+                        fs.Close()
+
+                        Me.Close()
+                        MainWindow.UpdateModGrid()
+                    End If
+                Catch ex As Exception
+
+                End Try
+            Else
+                Me.Close()
+                MsgBox("Mod already imported.")
+            End If
+
         End If
     End Sub
 End Class
